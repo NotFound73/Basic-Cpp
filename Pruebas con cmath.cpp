c7d4ffc8f83cf31d45f26983c78ff5546cfa7ea0
NotFound73 <quiquedese333@gmail.com>
2026-07-09 21:32:24 -0600

    uso de system ("cls") y system ("pause")

--- Pruebas con cmath.cpp
+++ Pruebas con cmath.cpp
@@ -6,10 +6,11 @@ using namespace std;
 void Pitagoras(){
     float opp, adj, hyp;
 
-    cout<<"Cateto opuesto: "; cin>>opp;
-    cout<<"Cateto adjacente: "; cin>>adj;
+    cout << "Cateto opuesto: "; cin >> opp;
+    cout << "Cateto adjacente: "; cin >> adj;
     hyp=sqrt(pow(opp, 2)+pow(adj, 2));
-    cout<<"Hipotenusa: "<<hyp;
+    cout<<"Hipotenusa: " << hyp<< endl;
+    system ("pause");
 }
 
 void Circulo(){
@@ -20,16 +21,26 @@ void Circulo(){
     area=pi*pow(r, 2);
     vol=(pi*pow(r, 3))*(4.0/3.0);
 
-    cout<<"\nArea: "<<area<<"\nVolumen: "<<vol;
+    cout << "\nArea: " << area << "\nVolumen: " << vol << endl;
+    system ("pause");
 }
 
 
 int main(){
     int x;
 
-    cout<<"¿Que se va a hacer?\n1.Teorema de Pitagoras\n2.Area/Volumen de un circulo\nSeleccionar: ";
-    cin>>x; cout<<"\n";
-    if (x==1){Pitagoras();}
-    else if (x==2){Circulo();}
-    else {cout<<"Numero invalido";}
+    do {
+        system ("cls");
+        cout<<"\n====================\nMenu de Operaciones\n====================" << endl << "\n1. Teorema de Pitagoras\n2. Area/Volumen de un circulo\n3. Terminar codigo\nSeleccionar(1, 2, 3): ";
+        cin>>x;
+        switch (x){
+            case 1: Pitagoras();
+            break;
+            case 2: Circulo();
+            break;
+            case 3: cout << "Codigo finalizado";
+            break;
+            default: cout << "Numero invalido";
+        }
+    } while (x!=3);
 }
