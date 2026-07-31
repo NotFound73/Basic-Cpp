11f2b7c82b883f73de18c0b1b53809bc0ef957f4
NotFound73 <quiquedese333@gmail.com>
2026-07-30 18:31:14 -0600

    Optimización de librerias

---
+++ Proyectos/Library managment/book.cpp
@@ -0,0 +1,54 @@
+#include "book.h"
+#include <iostream>
+using namespace std;
+
+book :: book(){
+    title = "Unknown";
+    author = "Unknown";
+    state = 2;
+}
+
+book :: book(string title, string author){
+    this -> title = title;
+    this -> author = author;
+    state = 1;
+}
+
+void book :: get_book(int i){
+         cout <<"\nBook Num. " << i+1 << endl;
+         cout << "Title: " << title << endl; 
+         cout << "Author: " << author << endl;
+         switch (state){
+            case 0: cout << "State: rented";
+            break;
+            case 1: cout << "State: available";
+            break;
+            case 2: cout << "Inexistent book";
+            break;
+            default: cout << "ERROR";
+         }
+         cout << endl;
+    }
+
+void book :: change_book_state(){
+        if (state == 1){
+            state = 0;
+            cout << "Book rented" << endl;
+        }
+        else if (state == 0){
+            state = 1;
+            cout << "Book returned" << endl;
+        }
+        else {
+            cout << "ERROR: Inexistent book" << endl;
+        }
+    }
+
+book set_book(int i){
+    string title, author;
+    cout << "Book " << i+1 << endl;
+    cout << "Title: "; getline (cin, title);
+    cout << "Author: "; getline (cin, author);
+    book newbook(title, author);
+    return newbook;
+}
